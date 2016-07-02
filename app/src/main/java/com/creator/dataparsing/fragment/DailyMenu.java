695870230ac57235b517b83c6357c17ed8b6c2f7
Sumeeth <sumeethgowda@gmail.com>
2016-07-02 11:29:41 +0530

    created fragments

---
+++ app/src/main/java/com/creator/dataparsing/fragment/DailyMenu.java
@@ -0,0 +1,11 @@
+package com.creator.dataparsing.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by admin on 02/07/16.
+ */
+public class DailyMenu extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/creator/dataparsing/fragment/DesertsMenu.java
@@ -0,0 +1,9 @@
+package com.creator.dataparsing.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by admin on 02/07/16.
+ */
+public class DesertsMenu extends Fragment {
+}
